f4269b97a350d26e45ffa487d48ae9dc887757c2
Adnan <adnanbilac@gmail.com>
2019-08-07 00:52:29 +0300

    Precompile false

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Myapp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.autoload_paths += %W(#{config.root}/lib)
+    config.assets.initialize_on_precompile = false
   end
 end
